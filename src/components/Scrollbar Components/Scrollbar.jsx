b102db75912b81e30e73a84cbb836371852e98e3
dathaker123 <dathakerxdd@gmail.com>
2023-04-15 04:07:55 -0400

    _

--- src/components/Scrollbar Components/Scrollbar.jsx
+++ src/components/Scrollbar Components/Scrollbar.jsx
@@ -1,22 +1,11 @@
 import React from "react";
 import styles from "./Scrollbar.module.css";
-import { useState } from "react";
 import ItemSVG from "./ItemSVG";
-import Modal from "./Modal";
-import ItemEditForm from "./ItemEditForm";
 import ItemAdd from "./ItemAdd";
 
-function Scrollbar({ startingItemList, editable , update}) {
-  const [ModalData, setModalData] = useState({
-    isVisible: false,
-    color: "default",
-    shape: "default",
-  });
-  function hideModalHandler() {
-    setModalData({ isVisible: false, color: "default", shape: "default" });
-  }
+function Scrollbar({ itemList, editable, onShowModal, onAddItem }) {
   function onItemClicked(item, index) {
-    setModalData({
+    onShowModal({
       isVisible: true,
       color: item.color,
       shape: item.shape,
@@ -24,42 +13,8 @@ function Scrollbar({ startingItemList, editable , update}) {
     });
   }
 
-  const [itemList, setItemList] = useState(startingItemList);
-  function changeDataHandler(newData) {
-    const newItemList = [...itemList];
-    newItemList[ModalData.index] = newData;
-    setItemList(newItemList);
-    hideModalHandler();
-    update(newItemList);
-  }
-  function deleteItemHandler() {
-    const newItemList = [...itemList];
-    newItemList.splice(ModalData.index, 1);
-    setItemList(newItemList);
-    hideModalHandler();
-    update(newItemList);
-  }
-  function addItemHandler() {
-    const newItemList = [...itemList];
-    newItemList.push({ color: "default", shape: "default" });
-    setItemList(newItemList);
-    hideModalHandler();
-    update(newItemList);
-  }
-
   return (
     <>
-      {ModalData.isVisible && (
-        <Modal onClose={hideModalHandler}>
-          <ItemEditForm
-            onCancel={hideModalHandler}
-            onEditItem={changeDataHandler}
-            onDeleteItem={deleteItemHandler}
-            defaultColor={ModalData.color}
-            defaultShape={ModalData.shape}
-          />
-        </Modal>
-      )}
       <div className={styles.scrollcontainer}>
         <div className={styles.buttoncontainer}>
           {itemList.map((item, index) => (
@@ -73,7 +28,7 @@ function Scrollbar({ startingItemList, editable , update}) {
               <p>{index + 1}</p>
             </button>
           ))}
-          {editable && <ItemAdd onItemAdd={addItemHandler} />}
+          {editable && <ItemAdd onItemAdd={onAddItem} />}
         </div>
       </div>
     </>
