3b7b2258d8e9e212b121374edcd972ce7cdd24e4
malekoa <azadeganmalek@gmail.com>
2023-10-29 11:31:47 -0400

    tailwindcss, bootstrapicons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<div class="w-full h-screen bg-gray-100">
+	<nav class="w-full h-14 bg-gray-300">
+		<div class="flex items-center justify-between h-full px-2 mx-auto max-w-7xl">
+			<div class="flex items-center">
+				<a href="/" class="flex items-center">
+					<!-- <img src="/images/logo.png" alt="logo" class="w-10 h-10" /> -->
+					<span class="ml-2 text-xl font-bold text-gray-800">Search & Change</span>
+				</a>
+			</div>
+			<div class="flex items-center">
+				<a href="/#" class="flex w-10 h-10 justify-center items-center text-white bg-gray-800 rounded-md hover:bg-gray-700">
+					<i class="bi bi-gear-wide text-2xl" />
+				</a>
+			</div>
+		</div>
+	</nav>
+</div>
